01aca8811c91e73f6a99a2a817170e6c985b914e
Tameka Roberson <ttrobersoncreate@gmail.com>
2024-06-26 20:12:28 -0400

    Add Contact Script Completed

--- Module-3-Final.py
+++ Module-3-Final.py
@@ -1,16 +1,30 @@
 import os
 import re
 
-names = {}
-phone_num = {}
-emails = {}
+contact_list = {}
+
+user_name = input("Hello user! May I get your first name? ").capitalize()
 
 def add_contact():
-    pass
+    email_pattern = r"\b[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{3}\b"
+    ind_num = 0 + 1
+    contact_list["Contact"] = {}
+    user_contact_name = input("Contact's Name: ").title()
+    user_contact_number = input("Contact's Phone No.: ")
+    user_email = input("Contact's E-mail Address: ")
+
+    if len(user_contact_number) == 10 and user_contact_number.isnumeric() and re.findall(email_pattern, user_email):
+        contact_list[f"Contact {ind_num}"] = {"Name" : user_contact_name, "Phone Number" : user_contact_number, "Email Address" : user_email}   
+
+    return f"{user_name}, {user_contact_name}'s contact information was saved."
+
 def edit_contact():
     pass
 def delete_contact():
-    pass
+    try:
+        pass
+    except ValueError:
+        pass
 def search_contact():
     pass
 def display_contact():
@@ -18,27 +32,31 @@ def display_contact():
 def export_contact():
     pass
 
-user_name = input("Hello user! May I get your first name? ").capitalize()
+
 
 user_start = input(f"It's nice to meet you {user_name}. Do you wish to use the Contact Management System? Y/N: ").capitalize()
 if user_start == "Y":
-    print("\nWelcome to the Contact Management System!\n\tMenu\n\t____\n1. Add Contact\n2. Edit Existing Contact\n3. Delete Contact\n4. Search For a Contact\n5. Display Contacts\n6. Export Contacts\n8. Exit")
-    user_choice = input("Please enter a number that corresponse with your inquiry: ")
-    match user_choice:
-        case 1:
-            pass
-        case 2:
-            pass
-        case 3:
-            pass
-        case 4:
-            pass
-        case 5:
-            pass
-        case 6:
-            pass
-        case 7:
-            print(f"Thank you for using the Contact Management System {user_name}! Goodbye")
+    while True:
+        print("\nWelcome to the Contact Management System!\n\tMenu\n\t____\n1. Add Contact\n2. Edit Existing Contact\n3. Delete Contact\n4. Search For a Contact\n5. Display Contacts\n6. Export Contacts\n8. Exit")
+        try:
+            user_choice = int(input("Please enter a number that corresponse with your inquiry: "))
+            match user_choice:
+                case 1:
+                    print(add_contact())
+                case 2:
+                    pass
+                case 3:
+                    pass
+                case 4:
+                    pass
+                case 5:
+                    pass
+                case 6:
+                    pass
+                case 7:
+                    print(f"Thank you for using the Contact Management System {user_name}! Goodbye")
+        except ValueError:
+            print(f"{user_name}, please select a number between 1-7...")
 elif user_start == "N":
     print(f"Have a nice day {user_name}!")
 else:
